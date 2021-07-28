194886c5240c2a4250592160bae8745b05e332df
sunipate <sunipate@SUNIPATE-IN.in.oracle.com>
2021-07-28 22:23:47 +0530

    adding swagger doc dependency

--- src/main/java/com/spatel/controller/SeriveOneController.java
+++ src/main/java/com/spatel/controller/SeriveOneController.java
@@ -1,8 +1,5 @@
 package com.spatel.controller;
 
-import java.util.Collections;
-import java.util.Map;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -18,29 +15,28 @@ import net.minidev.json.JSONObject;
 
 @RestController
 public class SeriveOneController {
-public static String serviceTwoUrl = "http://localhost:8081/hello";
-public static String serviceThreeUrl = "http://localhost:8082/hello";
-	
- @GetMapping("/healthCheck")
- public String checkService() {
-	 return "Up";
- }
- 
- @PostMapping("/hello")
- public String sayHello(@RequestBody FullName fullName) {
-	RestTemplate restTemplate = new RestTemplate();
-	HttpHeaders headers = new HttpHeaders();
-	headers.setContentType(MediaType.APPLICATION_JSON);
-	
-	String response1 = restTemplate.getForObject(serviceTwoUrl, String.class);
-	
-	JSONObject nameJsonObj = new JSONObject();
-	nameJsonObj.put("name", fullName.getName());
-	nameJsonObj.put("surName", fullName.getSurName());
-	HttpEntity<String> request = new HttpEntity<String>(nameJsonObj.toString(), headers);
-	String response2 = restTemplate.postForObject(serviceThreeUrl, request, String.class);
-    
-	 return response1 + " " + response2; 
-  }
- 
+	public static String serviceTwoUrl = "http://localhost:8081/hello";
+	public static String serviceThreeUrl = "http://localhost:8082/hello";
+
+	@GetMapping("/hello")
+	public String checkService() {
+		return "Up";
+	}
+
+	@PostMapping("/hello")
+	public String sayHello(@RequestBody FullName fullName) {
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		String response1 = restTemplate.getForObject(serviceTwoUrl, String.class);
+		JSONObject nameJsonObj = new JSONObject();
+		nameJsonObj.put("name", fullName.getName());
+		nameJsonObj.put("surName", fullName.getSurName());
+		HttpEntity<String> request = new HttpEntity<String>(nameJsonObj.toString(), headers);
+
+		String response2 = restTemplate.postForObject(serviceThreeUrl, request, String.class);
+		return response1 + " " + response2;
+	}
+
 }
--- src/main/java/com/spatel/demo/DemoApplication.java
+++ src/main/java/com/spatel/demo/DemoApplication.java
@@ -3,17 +3,29 @@ package com.spatel.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.spatel.controller", "com.spatel.services"})
 @EntityScan("com.spatel.jpa.entity")
 @EnableJpaRepositories(basePackages = {"com.spatel.jpa.repositories"})
+@EnableSwagger2
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).select()
+	         .apis(RequestHandlerSelectors.basePackage("com.spatel.controller")).build();
+	}
 }
